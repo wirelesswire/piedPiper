b352558c8a411ea51cf6b422ae433e1be6a991fc
wire <brnrml007@gmail.com>
2025-04-14 16:49:27 +0200

    should work just fine

--- FuzzySharp/MembershipFunctions/BaseMembershipFunction.cs
+++ FuzzySharp/MembershipFunctions/BaseMembershipFunction.cs
@@ -6,19 +6,51 @@
 
         protected BaseMembershipFunction(List<T> value) { }
 
+        private double fuzzy_Level = 1;
+
         public abstract List<T> Introduce();
 
-        public T GetMembership(T x) 
+        public T GetMembership(T x)
         {
-            T membershipValue = CalculateMembership(x); 
+            T membershipValue = CalculateMembership(x);
 
-            if (membershipValue < T.Zero || membershipValue > T.One)
-            {
-                throw new InvalidDataException($"Membership function must return a value between 0.0 and 1.0 (inclusive). Got {membershipValue}");
-            }
+            checkCorrectMembership(membershipValue);
 
-            return membershipValue; 
+            return calculateSharpenedValue(membershipValue);
         }
+        private void checkCorrectMembership(T x)
+        {
+            if (x < T.Zero || x > T.One)
+            {
+                throw new InvalidDataException($"Membership function must return a value between 0.0 and 1.0 (inclusive). Got {x}");
+            }
+        }
+        private T calculateSharpenedValue(T x)
+        {
+            if (fuzzy_Level == 1)
+            {
+                return x;
+            }
+            else
+            {
+                double z = Math.Pow(Convert.ToDouble(x), fuzzy_Level);
+                return T.CreateChecked(z); // Fix: Use T.CreateChecked to convert double to T  
+            }
+        }
+        public void sharpen(double factor)
+        {
+            fuzzy_Level *= factor;
+        }
+        public void fuzzyfi(double factor )
+        {
+            fuzzy_Level /= factor;
+        }
+        public void setFuzzyLevel(double factor)
+        {
+            fuzzy_Level = factor;
+        }
+
+
 
         protected abstract T CalculateMembership(T x);
 
@@ -26,7 +58,7 @@
 
         protected void ValidateArgs<I>(List<T> args, int count) where I : INumber<I>
         {
-            if (args.Count != count) 
+            if (args.Count != count)
             {
                 throw new InvalidDataException($"Invalid number of args provided {args.Count} should {count}");
             }
